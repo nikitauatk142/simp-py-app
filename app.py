120802910591775cd9496bb7f726a2a02433c03c
nikitauatk142 <52998415+nikitauatk142@users.noreply.github.com>
2022-06-13 13:55:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,14 @@
-import logging
-import json
-import requests
 import os
-from flask import Flask, render_template, request, redirect
-
-# Setup up a Flask instance
+from flask import Flask
 app = Flask(__name__)
 
-# Retrieve the time from an open API
-def retrieve_time():
-    try:
-        response = requests.get(
-            url="http://worldclockapi.com/api/json/utc/now"
-        )
-        if response.status_code == 200:
-            time = (json.loads(response.text))['currentDateTime']
-            return time
-        else:
-            logging.error(f"Error querying API.  Status code: {response.status_code}")
-    except Exception:
-        return "Retrieving time failed"
-
-# Render the template
 @app.route("/")
-def index():
-    return render_template('index.html')
+def main():
+    return "Welcome!"
 
-# Make functions available to web template
-app.jinja_env.globals.update(retrieve_time=retrieve_time)
+@app.route('/how are you')
+def hello():
+    return 'I am good, how about you?'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
